e5802f6964f8621521fdf0bd4dbd04961e52fed3
Anoosha Naz <163389690+anooshanaz@users.noreply.github.com>
2024-08-23 17:51:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+import inquirer from "inquirer";
+class myAccount {
+    name;
+    mybalance;
+    mypin;
+    constructor(name, mybalance, mypin) {
+        this.mybalance = mybalance;
+        this.mypin = mypin;
+        this.name = name;
+        console.log("welcome to mybank");
+        console.log("your current Account balance is 20000");
+    }
+}
+let answer = new myAccount("kiran", 20000, 2345);
+let password = await inquirer.prompt([
+    {
+        name: "Person",
+        type: "string",
+        message: "please enter your name"
+    },
+    {
+        name: "Amount",
+        type: "number",
+        message: "please enter your Amount"
+    },
+    {
+        name: "pin",
+        type: "number",
+        message: "please enter your pin"
+    }
+]);
+if (password.pin === 2345) {
+    console.log("Authentication is successfully");
+}
+else {
+    console.log("Invalid pin");
+}
+if (password.Amount === 20000) {
+    console.log("withdraw your amount");
+}
+else if (password.Amount > 20000) {
+    console.log("Insufficient balance");
+}
+else if (password.Amount < 20000) {
+    console.log("Insufficient balance");
+}
+else {
+    console.log("Invalid Amount");
+}
